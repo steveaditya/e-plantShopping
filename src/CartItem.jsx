ba6825084545c383cee4b6d4ea42926367534f2a
steveaditya <steve.aditya9976@gmail.com>
2024-10-07 04:51:30 -0400

    almost done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -14,9 +14,9 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart}) => {
         }, 0).toFixed(2);
     };
 
-    const handleContinueShopping = (e) => {
-        onContinueShopping();
-    };
+    // const handleContinueShopping = (e) => {
+    //     onContinueShopping();
+    // };
 
 
 
@@ -40,14 +40,14 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart}) => {
 
     // Calculate total cost based on quantity for an item
     const calculateTotalCost = (item) => {
-        return (parseFloat(item.cost.replace('$', '')) * item.quantity).toFixed(2);
+        return (parseFloat(item.cost.slice(1)) * item.quantity).toFixed(2);
     };
 
     return (
         <div className="cart-container">
             <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
             <div>
-                {cart.map(item => (
+                {cart.map((item) => (
                     <div className="cart-item" key={item.name}>
                         <img className="cart-item-image" src={item.image} alt={item.name} />
                         <div className="cart-item-details">
@@ -66,7 +66,7 @@ const CartItem = ({ onContinueShopping, onRemoveFromCart}) => {
             </div>
             <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
             <div className="continue_shopping_btn">
-                <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+                <button className="get-started-button" onClick={onContinueShopping}>Continue Shopping</button>
                 <br />
                 <button className="get-started-button1">Checkout</button>
             </div>
